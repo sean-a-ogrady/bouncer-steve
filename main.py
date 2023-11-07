44ba622f0f67f70f18412c31c5e0eb6ab18a3344
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-11-07 16:33:07 -0500

    Set up project architecture

---
+++ main.py
@@ -0,0 +1,16 @@
+# Workflow for libraries
+# pip install <libarary name>
+# pip freeze > requirements.txt
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def main():
+    # Initialize managers
+    # context, discord, mood, openai, rcon, relationship
+    pass
+
+if __name__ == '__main__':
+    main()
